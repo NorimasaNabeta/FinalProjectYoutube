3e4e991ef3f9163e2685c6ee78aba1067a1b3650
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-08-31 22:34:47 +0900

    add python script file and input text data usage:   % python ffplist_create.py

---
+++ FinalProjectYoutube/ffplist_create.py
@@ -0,0 +1,44 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Time-stamp: <2012-08-31 22:31:46 NorimasaNabeta>
+#
+
+import plistlib
+import datetime
+import time
+import re
+
+def readTextData( filename ):
+    tags = [ 'app', 'youtube', 'name', '' ]
+    f = open(filename)
+    data = f.read()
+    f.close()
+
+    ctclass = []
+    tmp = {}
+    state=0
+    for line in data.split('\n'):
+        if ( len(line) > 0 ):
+            if (re.match('^@', line)):
+                if (state > 2):
+                    ctclass.append(tmp)
+                tmp = {}
+                state = 0
+            elif (re.match('^#', line)):
+                tmp={}
+            else:
+                tmp[ tags[state] ] = line
+                state = state + 1
+    return ctclass
+
+if __name__ == '__main__':
+    output_pl = dict(
+        title = "Coding Together Summer2012",
+        description = "Coding Together: Apps for iPhone and iPad", 
+        date = datetime.datetime.fromtimestamp(time.mktime(time.gmtime())),
+        version = 1,
+        students = readTextData('studentlist.txt') 
+        )
+    plistlib.writePlist(output_pl, 'CodingTogetherSummer2012.plist')
+
+
