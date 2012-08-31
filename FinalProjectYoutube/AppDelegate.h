6ecb16882466ed846de297144edf7ae2976a9cb7
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-08-31 10:52:03 +0900

    Initial Commit

---
+++ FinalProjectYoutube/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinalProjectYoutube
+//
+//  Created by Norimasa Nabeta on 2012/08/31.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FinalProjectYoutube/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FinalProjectYoutube
+//
+//  Created by Norimasa Nabeta on 2012/08/31.
+//  Copyright (c) 2012年 Norimasa Nabeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
