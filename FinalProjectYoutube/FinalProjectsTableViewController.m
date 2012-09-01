ee9b3fbfeab9655dfed159f00b3aacef2a5b0c70
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-09-01 14:48:31 +0900

    thumbnail fetching only once

--- FinalProjectYoutube/FinalProjectsTableViewController.m
+++ FinalProjectYoutube/FinalProjectsTableViewController.m
@@ -29,17 +29,20 @@
 
 - (void) preFetchThumbnails
 {
+    UIImage *dummy = [UIImage imageNamed:@"crimson.png"];
     id appDelegate = (id)[[UIApplication sharedApplication] delegate];
     for (NSDictionary *entry in self.students) {
         NSString *videoID = [entry objectForKey:@"youtube"];
         UIImage *image = [[appDelegate imageCache] objectForKey:videoID];
         if (! image) {
+            [[appDelegate imageCache] setObject:dummy forKey:videoID];
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
                 NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://img.youtube.com/vi/%@/2.jpg", videoID]];
                 NSData *data = [NSData dataWithContentsOfURL:url];
                 UIImage *image = [UIImage imageWithData:data];
                 dispatch_sync(dispatch_get_main_queue(), ^{
                     [[appDelegate imageCache] setObject:image forKey:videoID];
+                    [self.tableView reloadData];
                 });
             });
         }
@@ -85,15 +88,17 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
     NSDictionary *entry = [self.students objectAtIndex:indexPath.row];
-    NSString *videoID = [entry objectForKey:@"youtube"];
-    NSString *appID = [entry objectForKey:@"app"];
+    NSString *videoID   = [entry objectForKey:@"youtube"];
+    NSString *appID     = [entry objectForKey:@"app"];
     cell.textLabel.text = [entry objectForKey:@"name"];
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%@ [%@]", appID,videoID];
    
+    UIImage *dummy = [UIImage imageNamed:@"crimson.png"];
     id appDelegate = (id)[[UIApplication sharedApplication] delegate];
     UIImage *image = [[appDelegate imageCache] objectForKey:videoID];
     if (image) { cell.imageView.image = image;}
     else {
+        [[appDelegate imageCache] setObject:dummy forKey:videoID];
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://img.youtube.com/vi/%@/2.jpg", videoID]];
             NSData *data = [NSData dataWithContentsOfURL:url];
@@ -101,14 +106,13 @@
             dispatch_sync(dispatch_get_main_queue(), ^{
                 [[appDelegate imageCache] setObject:image forKey:videoID];
                 cell.imageView.image = image;
+                [self.tableView reloadData];
             });
         });
     }
     
     return cell;
 }
-
-// http://img/youtube/com/vi/Q-6HiabgIKc/2.jpg
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
