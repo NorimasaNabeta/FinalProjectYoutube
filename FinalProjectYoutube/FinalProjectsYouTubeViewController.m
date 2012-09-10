79f62dae5643aeaeae00642f73a8c34dd98b10b8
Norimasa Nabeta <nabeta@mars.dti.ne.jp>
2012-09-10 14:30:07 +0900

    fix rotation behavior to show youtube

--- FinalProjectYoutube/FinalProjectsYouTubeViewController.m
+++ FinalProjectYoutube/FinalProjectsYouTubeViewController.m
@@ -18,12 +18,8 @@
 //
 // http://stackoverflow.com/questions/6283079/iphone-uiwebview-embed-youtube-video
 //
-- (void) setStudent:(NSDictionary *)student
+- (void) reloadContent
 {
-    if (_student != student) {
-        _student=student;
-    }
-    self.title = [_student objectForKey:@"name"];
     NSString *embedHTML2 = @"<html><head><style type=\"text/css\">\
     body { background-color: transparent;color: transparent;}</style>\
     </head><body style=\"margin:0\">\
@@ -31,11 +27,20 @@
     width=\"%0.0f\" height=\"%0.0f\"></embed>\
     </body></html>";
     NSString *youtube=[self.student objectForKey:@"youtube"];
-    // NSRange range=[youtube rangeOfString:@"http"];
-    // if(range.location == NSNotFound){}
+
+    // NSLog(@"width=%g height=%g", self.view.frame.size.width, self.view.frame.size.height);
     NSString *html = [NSString stringWithFormat:embedHTML2, youtube,
-            self.view.frame.size.width, self.view.frame.size.height];
+                      self.view.frame.size.width, self.view.frame.size.height];
     [self.webView loadHTMLString:html baseURL:[NSURL URLWithString:@"http://youtube.com"]];
+}
+
+- (void) setStudent:(NSDictionary *)student
+{
+    if (_student != student) {
+        _student=student;
+    }
+    self.title = [_student objectForKey:@"name"];
+    [self reloadContent];
 }
 
 
@@ -52,6 +57,11 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [self reloadContent];
 }
 
 - (void)viewDidUnload
